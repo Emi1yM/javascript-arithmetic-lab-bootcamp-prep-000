5811ec45d112c0050b5afecb16a12d8400ee77cf
upbeat-scope-9442 <mle.jo.11@gmail.com>
2019-06-19 17:17:00 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,11 +5,14 @@ function subtract(a,b){
   return a - b;
 }
 function multiply(a,b){
-  return a * b
+  return a * b;
 }
 function divide(a,b){
-  return a / b
+  return a / b;
 }
 function inc(n){
-  return ++n
+  return ++n;
+}
+function dec(n){
+  return --n;
 }
